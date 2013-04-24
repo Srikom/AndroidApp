31ab1ac639e6d6ff13ad61beba9c5f5ae685ed07
Ian <ian901223@gmail.com>
2013-04-24 22:59:04 +0800

    Modified files

--- src/com/example/androidapp/Splash.java
+++ src/com/example/androidapp/Splash.java
@@ -1,6 +1,7 @@
 package com.example.androidapp;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 
 public class Splash extends Activity{
@@ -9,8 +10,29 @@ public class Splash extends Activity{
 	protected void onCreate(Bundle savedInstanceState) {
 		// TODO Auto-generated method stub
 		super.onCreate(savedInstanceState);
+		setContentView(R.layout.splash);
 		
-		
+		Thread timer = new Thread(){
+			
+			public void run()
+			{
+				try
+				{
+					sleep(3000);
+				}
+				catch(InterruptedException e)
+				{
+					e.printStackTrace();
+				}
+				finally
+				{
+					Intent newIntent = new Intent("com.example.androidapp.MAINACTIVITY");
+					startActivity(newIntent);
+				}
+			}
+			
+		};
+		timer.start();
 	}
 	
 	
