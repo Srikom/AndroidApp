ca8cabb7088c15f3894c46ef7819ff2a09926f3b
Ian <ian901223@gmail.com>
2013-04-25 00:51:47 +0800

    Added onPause Method

--- src/com/example/androidapp/Splash.java
+++ src/com/example/androidapp/Splash.java
@@ -34,6 +34,14 @@ public class Splash extends Activity{
 		};
 		timer.start();
 	}
+
+	@Override
+	protected void onPause() {
+		// TODO Auto-generated method stub
+		super.onPause();
+		finish();
+	}
+	
 	
 	
 
