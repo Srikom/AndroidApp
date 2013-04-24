0641bde3d0ab3798ae8dd4fab91db05f95a247bf
Ian <ian901223@gmail.com>
2013-04-24 22:46:56 +0800

    Change mainActivity.java

--- src/com/example/androidapp/MainActivity.java
+++ src/com/example/androidapp/MainActivity.java
@@ -3,13 +3,41 @@ package com.example.androidapp;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends Activity {
-
+	int counter = 0;
+	TextView tv;
+	Button add,sub;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		
+		tv = (TextView) findViewById(R.id.tv);
+		add = (Button) findViewById(R.id.addB);
+		sub = (Button) findViewById(R.id.subB);
+		
+		add.setOnClickListener(new View.OnClickListener() {
+			
+			@Override
+			public void onClick(View arg0) {
+				counter++;
+				tv.setText("The Total Value is "+counter);
+			}
+		});
+		
+		sub.setOnClickListener(new View.OnClickListener() {
+			
+			@Override
+			public void onClick(View arg0) {
+				counter--;
+				tv.setText("The Total Value is "+counter);
+			}
+		});
+		
 	}
 
 	@Override
